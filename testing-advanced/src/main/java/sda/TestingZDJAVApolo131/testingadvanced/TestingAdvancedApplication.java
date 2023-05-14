903d9b6adf60bd9b7abaabf670ff0d086d759945
Maciej Burzyński <maciej.kollaps@gmail.com>
2023-05-14 10:26:19 +0200

    Init Spring project for testing advanced classes

---
+++ testing-advanced/src/main/java/sda/TestingZDJAVApolo131/testingadvanced/TestingAdvancedApplication.java
@@ -0,0 +1,13 @@
+package sda.TestingZDJAVApolo131.testingadvanced;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingAdvancedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingAdvancedApplication.class, args);
+	}
+
+}
---
+++ testing-advanced/src/test/java/sda/TestingZDJAVApolo131/testingadvanced/TestingAdvancedApplicationTests.java
@@ -0,0 +1,13 @@
+package sda.TestingZDJAVApolo131.testingadvanced;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingAdvancedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
