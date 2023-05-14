d0166b3725c17ce39c58451ee19a290ed4df68fe
Maciej Burzyński <maciej.kollaps@gmail.com>
2023-05-14 11:52:02 +0200

    Add AssertJ assertions

--- testing-advanced/src/test/java/sda/TestingZDJAVApolo131/testingadvanced/tdd/CalculatorTest.java
+++ testing-advanced/src/test/java/sda/TestingZDJAVApolo131/testingadvanced/tdd/CalculatorTest.java
@@ -1,22 +1,38 @@
 package sda.TestingZDJAVApolo131.testingadvanced.tdd;
 
 
+import org.assertj.core.api.Assertions;
+import org.assertj.core.data.Percentage;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CalculatorTest {
 
+    Calculator calculator;
+
+    @BeforeEach
+    void initCalculator(){
+        calculator = new Calculator();
+    }
 
     @Test
     void whenAddTwoNumbersThenReturnSumOfTheNumbers(){
 //    void givenCalculatorWhenAddTwoNumbersThenReturnSumOfTheNumbers(){ <- with given
 //    given
-        Calculator calculator = new Calculator();
+        // Calculator initiated in @BeforeEach
 //    when
         int result = calculator.add(2,3);
 //    then
         assertEquals(5, result);
         assertNotEquals(4, result);
+        Assertions.assertThat(result).isPositive(); // AssertJ
+        Assertions.assertThat(result).isGreaterThan(2); // AssertJ
+        Assertions.assertThat(result).isNotZero(); // AssertJ
+        Assertions.assertThat(result).isCloseTo(4, Percentage.withPercentage(90)); // AssertJ
     }
+    
+//    add tests for subtraction in our Calculator
+
 }
